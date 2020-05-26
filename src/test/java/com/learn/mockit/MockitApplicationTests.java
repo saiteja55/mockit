74b38ba8af58f9dab7ed1f625534b699c76a1e34
saiteja55 <saitejareddy55@gmail.com>
2020-05-26 14:03:35 +0530

    First Commit

---
+++ src/test/java/com/learn/mockit/MockitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.mockit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
